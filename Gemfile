tohosaku/redmine-dev-mirror

Add sample gem plugin

Gemfile
@@ -114,3 +114,14 @@ end
 Dir.glob File.expand_path("../plugins/*/{Gemfile,PluginGemfile}", __FILE__) do |file|
   eval_gemfile file
 end
+
+plugin_gemfile = File.join(File.dirname(__FILE__), "Gemfile.plugin")
+if File.exist?(plugin_gemfile)
+  eval_gemfile plugin_gemfile
+end
+
+if ENV['RAILS_ENV'] == 'test'
+  group :plugin do
+    gem 'baz_plugin', :path => 'test/fixtures/gem_plugins/baz_plugin'
+  end
+end

tohosaku/redmine

Gemfile
@@ -117,3 +117,14 @@ end
 Dir.glob File.expand_path("../plugins/*/{Gemfile,PluginGemfile}", __FILE__) do |file|
   eval_gemfile file
 end
+
+extension_gemfile = File.join(File.dirname(__FILE__), "Gemfile.extension")
+if File.exist?(extension_gemfile)
+  eval_gemfile extension_gemfile
+end
+
+if ENV['RAILS_ENV'] == 'test'
+  group :extension do
+    gem 'baz_plugin', :path => 'test/fixtures/gem_plugins/baz_plugin'
+  end
+end