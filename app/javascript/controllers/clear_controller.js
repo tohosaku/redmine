3dc3792fd950b35c1636a45f97f08a3587539c2e
Takashi Kato <tohosaku@users.osdn.me>
2022-11-25 22:54:22 +0000

    Add stimulus clear controller

--- app/javascript/controllers/clear_controller.js
+++ app/javascript/controllers/clear_controller.js
@@ -7,7 +7,7 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
 
-  static targets = ['element'];
+  static targets = ['element', 'clear', 'child', 'dummy'];
 
   clearIfChecked(e) {
     if (!e.target.checked) return
@@ -15,5 +15,15 @@ export default class extends Controller {
     this.elementTargets.forEach(element => {
       element.value = ''
     })
+  }
+
+  clear(e) {
+    this.clearTargets.forEach(elm => elm.value = '');
+    this.childTargets.forEach(elm => elm.replaceChildren());
+  }
+
+  dummyTargetConnected(element) {
+    this.clear();
+    element.remove();
   }
 }
