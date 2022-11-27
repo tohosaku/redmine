3b67f3295d2cd4ce40063639d87e0df199fcc8ff
Takashi Kato <tohosaku@users.osdn.me>
2022-11-27 08:42:22 +0000

    Convert SJR to Turbo Streams in mypage view

--- app/controllers/my_controller.rb
+++ app/controllers/my_controller.rb
@@ -171,7 +171,7 @@ class MyController < ApplicationController
       @user.pref.save
       respond_to do |format|
         format.html {redirect_to my_page_path}
-        format.js
+        format.turbo_stream
       end
     else
       render_error :status => 422
@@ -187,7 +187,7 @@ class MyController < ApplicationController
     @user.pref.save
     respond_to do |format|
       format.html {redirect_to my_page_path}
-      format.js
+      format.turbo_stream
     end
   end
 
