2ad21376cbbd901f9d0b0fbde9ba4253d236967e
Takashi Kato <tohosaku@users.osdn.me>
2022-12-15 12:39:29 +0000

    Convert SJR to Turbo Streams in issue view

---
+++ app/javascript/controllers/issue_controller.js
@@ -0,0 +1,52 @@
+import { Controller } from "@hotwired/stimulus"
+import { post } from '@rails/request.js'
+
+// Connects to data-controller="issue"
+export default class extends Controller {
+
+  static values = {
+    url: String
+  }
+
+  static targets = ["form", "category", "attribute", "trigger"]
+
+  update(e) {
+    this.allAttributes.forEach(element => {
+      element.dataset.valuebeforeupdate = element.value
+    });
+    if (e.currentTarget) {
+      this.triggerTarget.value = e.currentTarget.id
+    }
+
+    return post(this.urlValue, {
+      body: new FormData(this.element),
+      responseKind: 'turbo-stream'
+    });
+  }
+
+  replace(replacement) {
+    this.allAttributes.forEach(element => {
+      const object_id = element.id;
+      if (object_id && element.dataset.valuebeforeupdate != element.value) {
+        replacement.querySelector('#'+object_id).value = element.value;
+      }
+    });
+    this.attributeTarget.innerHTML = ''
+    this.attributeTarget.append(replacement)
+  }
+
+  get allAttributes() {
+    return this.attributeTarget.querySelectorAll("input, textarea, select")
+  }
+
+  formTargetConnected(element) {
+    const clone = element.content.cloneNode(true);
+    this.replace(clone);
+    element.remove();
+  }
+
+  categoryTargetConnected(element) {
+    $('#issue_assigned_to_id').find('option').first().html(element.innerHTML)
+    element.remove();
+  }
+}
