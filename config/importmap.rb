aee48ce31e6637d13509d5aa4a1a7ccf8a85d619
tohosaku <tohosaku@users.osdn.me>
2024-01-15 04:08:03 +0000

    Add turbo-rails

--- config/importmap.rb
+++ config/importmap.rb
@@ -11,3 +11,4 @@ pin "@redmine-ui/tribute", to: "tribute-6.0.0.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
