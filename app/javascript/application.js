3c1e92777b532145c072f930caab4dcda98ead40
tohosaku <tohosaku@users.osdn.me>
2024-01-15 04:08:03 +0000

    Add turbo-rails

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,8 @@
 import "controllers"
+import "@hotwired/turbo-rails"
 import {createTooltip} from 'tooltip';
+
+Turbo.session.drive = false;
 
 document.addEventListener('mouseover', (e) => {
   const tooltip = createTooltip()
