f86ea7e0b14e344bcbad2e772a502ecc59744a78
tohosaku <tohosaku@users.osdn.me>
2024-01-19 02:45:52 +0000

    Add checkbox controller

---
+++ app/javascript/controllers/checkbox_controller.js
@@ -0,0 +1,20 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="checkbox"
+export default class extends Controller {
+  toggle(e) {
+    e.preventDefault();
+
+    const elements = Array.from(this.element.querySelectorAll(e.params.selector));
+    const allChecked  = elements.every(el => el.checked);
+
+    elements.forEach(el => el.checked = !allChecked);
+    const event = new Event('change');
+    this.element.dispatchEvent(event);
+  }
+
+  checkAll(e) {
+    const elements = document.getElementById(e.params.id).querySelectorAll('input[type=checkbox]:enabled');
+    Array.from(elements).forEach(el => el.checked = e.params.checked);
+  }
+}
