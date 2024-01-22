f8cdb44ed42394861eeea01b4b7f070300a6d97a
tohosaku <tohosaku@users.osdn.me>
2024-01-22 07:53:23 +0000

    Add search--form controller

---
+++ app/javascript/controllers/search/form_controller.js
@@ -0,0 +1,32 @@
+import { Controller } from "@hotwired/stimulus"
+import { toggleExpandCollapseIcon } from 'helper'
+
+// Connects to data-controller="search--form"
+export default class extends Controller {
+  static targets = ['types', 'input', 'options', 'hidden']
+
+  submitForm(e) {
+    e.preventDefault();
+    const checkboxes = this.typesTarget.querySelectorAll('input[type=checkbox]')
+    checkboxes.forEach((e) => e.removeAttribute('checked'));
+
+    const checkbox = e.currentTarget.previousElementSibling;
+    checkbox.setAttribute('checked', 'checked');
+    if (this.inputTarget.value !== '') {
+        this.element.requestSubmit();
+    }
+  }
+
+  setVisibilityOfOptions(e) {
+    const showOptions = this.optionsTarget.style.display !== 'none' ? '1' : '0';
+    this.hiddenTarget.value = showOptions;
+  }
+}
+
+function toggleFieldset(el) {
+  var fieldset = $(el).parents('fieldset').first();
+  fieldset.toggleClass('collapsed');
+  fieldset.children('legend').toggleClass('icon-expanded icon-collapsed');
+  toggleExpandCollapseIcon(fieldset.children('legend')[0])
+  fieldset.children('div').toggle();
+}
