b2ada1f96bdf7cba3c910a3c7038066953652121
Takashi Kato <tohosaku@users.osdn.me>
2024-01-22 12:56:52 +0000

    Clear value when connected

--- app/javascript/controllers/clear_controller.js
+++ app/javascript/controllers/clear_controller.js
@@ -3,6 +3,16 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
 
   static targets = ["clear"];
+  static values = {
+    defaultclear: Boolean
+  }
+
+  connect() {
+    if (this.defaultclearValue) {
+      this.clear();
+    }
+  }
+
 
   clear(e) {
     this.clearTargets.forEach(elm => elm.value = '');
