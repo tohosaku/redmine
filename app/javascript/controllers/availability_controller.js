e28f032ae7554af39d7e7726c65652762af9c531
tohosaku <tohosaku@users.osdn.me>
2024-01-25 07:59:55 +0000

    Add issue_toggleable_field helper(uses some stimulus controller)

--- app/javascript/controllers/availability_controller.js
+++ app/javascript/controllers/availability_controller.js
@@ -8,6 +8,10 @@ export default class extends Controller {
     this.fieldTarget.disabled = !e.currentTarget.checked
   }
 
+  disableIfChecked(e) {
+    this.fieldTarget.disabled = e.currentTarget.checked
+  }
+
   toggle(e) {
     if (typeof e.params !== 'undefined') {
       this.fieldTarget.disabled = e.params.matched;
