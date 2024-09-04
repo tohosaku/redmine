dd087a7609126656e10c45fba6cae76399e83d30
Takashi Kato <tohosaku@users.osdn.me>
2024-09-04 23:02:37 +0000

    Add queries__columns method

--- app/javascript/controllers/queries/columns_controller.js
+++ app/javascript/controllers/queries/columns_controller.js
@@ -5,6 +5,30 @@ export default class extends Controller {
   moveOptions(e) {
     const from_id = e.params.from;
     const to_id   = e.params.to;
-    moveOptions(this.element.form[from_id], this.element.form[to_id]);
+    $(this.element.form[from_id]).find('option:selected').detach().prop("selected", false).appendTo($(this.element.form[to_id]));
+  }
+
+  moveOptionTop(e) {
+    const theSel = this.element.form[e.params.id];
+    $(theSel).find('option:selected').detach().prependTo($(theSel));
+  }
+
+  moveOptionUp(e) {
+    const theSel = this.element.form[e.params.id];
+    $(theSel).find('option:selected').each(function(){
+      $(this).prev(':not(:selected)').detach().insertAfter($(this));
+    });
+  }
+
+  moveOptionDown(e) {
+    const theSel = this.element.form[e.params.id];
+    $($(theSel).find('option:selected').get().reverse()).each(function(){
+      $(this).next(':not(:selected)').detach().insertBefore($(this));
+    });
+  }
+
+  moveOptionBottom(e) {
+    const theSel = this.element.form[e.params.id];
+    $(theSel).find('option:selected').detach().appendTo($(theSel));
   }
 }
