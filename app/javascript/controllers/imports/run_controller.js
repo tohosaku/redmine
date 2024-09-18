efce65300acc71769d19db415546785cb8843131
Takashi Kato <tohosaku@users.osdn.me>
2024-09-18 13:13:39 +0000

    Remove jQuery-ui progressbar

---
+++ app/javascript/controllers/imports/run_controller.js
@@ -0,0 +1,17 @@
+/**
+ * Redmine - project management software
+ * Copyright (C) 2006-  Jean-Philippe Lang
+ * This code is released under the GNU General Public License.
+ */
+
+import { Controller } from "@hotwired/stimulus"
+import { post } from '@rails/request.js'
+
+// Connects to data-controller="imports--run"
+export default class extends Controller {
+
+  connect() {
+    this.element.classList.add('ajax-loading');
+    post(this.element.dataset.runpath);
+  }
+}
