85e8ded9894a5d6bea17b4cc102656b33a513c6f
Takashi Kato <tohosaku@users.osdn.me>
2024-09-20 23:17:39 +0000

    [WIP] Add tab-buttons controller

--- app/javascript/controllers/tab_buttons_controller.js
+++ app/javascript/controllers/tab_buttons_controller.js
@@ -2,6 +2,9 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="tab-buttons"
 export default class extends Controller {
+  connect() {
+  }
+
   left(e) {
     e.preventDefault();
     moveTabLeft(e.currentTarget)
@@ -11,4 +14,63 @@ export default class extends Controller {
     e.preventDefault();
     moveTabRight(e.currentTarget)
   }
+}
+
+function moveTabRight(el) {
+  var lis = $(el).parents('div.tabs').first().find('ul').children();
+  var bw = $(el).parents('div.tabs-buttons').outerWidth(true);
+  var tabsWidth = 0;
+  var i = 0;
+  lis.each(function() {
+    if ($(this).is(':visible')) {
+      tabsWidth += $(this).outerWidth(true);
+    }
+  });
+  if (tabsWidth < $(el).parents('div.tabs').first().width() - bw) { return; }
+  $(el).siblings('.tab-left').removeClass('disabled');
+  while (i<lis.length && !lis.eq(i).is(':visible')) { i++; }
+  var w = lis.eq(i).width();
+  lis.eq(i).hide();
+  if (tabsWidth - w < $(el).parents('div.tabs').first().width() - bw) {
+    $(el).addClass('disabled');
+  }
+}
+
+function moveTabLeft(el) {
+  var lis = $(el).parents('div.tabs').first().find('ul').children();
+  var i = 0;
+  while (i < lis.length && !lis.eq(i).is(':visible')) { i++; }
+  if (i > 0) {
+    lis.eq(i-1).show();
+    $(el).siblings('.tab-right').removeClass('disabled');
+  }
+  if (i <= 1) {
+    $(el).addClass('disabled');
+  }
+}
+
+function displayTabsButtons() {
+  var lis;
+  var tabsWidth;
+  var el;
+  var numHidden;
+  $('div.tabs').each(function() {
+    el = $(this);
+    lis = el.find('ul').children();
+    tabsWidth = 0;
+    numHidden = 0;
+    lis.each(function(){
+      if ($(this).is(':visible')) {
+        tabsWidth += $(this).outerWidth(true);
+      } else {
+        numHidden++;
+      }
+    });
+    var bw = $(el).find('div.tabs-buttons').outerWidth(true);
+    if ((tabsWidth < el.width() - bw) && (lis.length === 0 || lis.first().is(':visible'))) {
+      el.find('div.tabs-buttons').hide();
+    } else {
+      el.find('div.tabs-buttons').show().children('button.tab-left').toggleClass('disabled', numHidden == 0);
+    }
+  });
 }
