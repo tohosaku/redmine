3dd8e3798205ed1a954927034fc20a8191520180
Takashi Kato <tohosaku@users.osdn.me>
2024-09-21 11:49:03 +0000

    Add radiobutton controller

--- app/javascript/controllers/focus_controller.js
+++ app/javascript/controllers/focus_controller.js
@@ -15,6 +15,13 @@ export default class extends Controller {
     }
   }
 
+  focusIfTrue(e) {
+    const element = document.getElementById(e.params.id)
+    if (e.detail && e.detail.checked) {
+      element.focus();
+    }
+  }
+
   dummyTargetConnected(element) {
     this.pointTarget.focus();
     element.remove();
