d6a6376344dca1e8f652b1cd1b5f848bbb18f908
Takashi Kato <tohosaku@users.osdn.me>
2025-02-19 01:55:03 +0000

    Convert project_identifier to stimulus controller

--- app/javascript/controllers/projects/identifier_controller.js
+++ app/javascript/controllers/projects/identifier_controller.js
@@ -4,10 +4,32 @@
  * This code is released under the GNU General Public License.
  */
 
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="projects--identifier"
 // Automatic project identifier generation
+export default class extends Controller {
+
+  static targets = ['identifier', 'name']
+
+  connect() {
+    this.locked = (this.identifierTarget.value != '');
+    this.maxlength = parseInt(this.identifierTarget.getAttribute('maxlength'));
+  }
+
+  setIdentifier(e) {
+    if(!this.locked) {
+      this.identifierTarget.value = generateProjectIdentifier(this.nameTarget.value, this.maxlength);
+    }
+  }
+
+  setLock(e) {
+    this.locked = (this.identifierTarget.value != '' &&  this.identifierTarget.value != generateProjectIdentifier(this.nameTarget.value, this.maxlength));
+  }
+}
 
 function generateProjectIdentifier(identifier, maxlength) {
-  var diacriticsMap = [
+  const diacriticsMap = [
     {'base':'a', 'letters':/[\u0061\u24D0\uFF41\u1E9A\u00E0\u00E1\u00E2\u1EA7\u1EA5\u1EAB\u1EA9\u00E3\u0101\u0103\u1EB1\u1EAF\u1EB5\u1EB3\u0227\u01E1\u01DF\u1EA3\u00E5\u01FB\u01CE\u0201\u0203\u1EA1\u1EAD\u1EB7\u1E01\u0105\u2C65\u0250\u0041\u24B6\uFF21\u00C0\u00C1\u00C2\u1EA6\u1EA4\u1EAA\u1EA8\u00C3\u0100\u0102\u1EB0\u1EAE\u1EB4\u1EB2\u0226\u01E0\u01DE\u1EA2\u00C5\u01FA\u01CD\u0200\u0202\u1EA0\u1EAC\u1EB6\u1E00\u0104\u023A\u2C6F]/g},
     {'base':'aa','letters':/[\uA733\uA732]/g},
     {'base':'ae','letters':/[\u00E4\u00E6\u01FD\u01E3\u00C4\u00C6\u01FC\u01E2]/g},
@@ -63,22 +85,3 @@ function generateProjectIdentifier(identifier, maxlength) {
   identifier = identifier.substr(0, maxlength); // max characters
   return identifier;
 }
-
-function autoFillProjectIdentifier() {
-  var locked = ($('#project_identifier').val() != '');
-  var maxlength = parseInt($('#project_identifier').attr('maxlength'));
-
-  $('#project_name').keyup(function(){
-    if(!locked) {
-      $('#project_identifier').val(generateProjectIdentifier($('#project_name').val(), maxlength));
-    }
-  });
-
-  $('#project_identifier').keyup(function(){
-    locked = ($('#project_identifier').val() != '' && $('#project_identifier').val() != generateProjectIdentifier($('#project_name').val(), maxlength));
-  });
-}
-
-$(document).ready(function(){
-  autoFillProjectIdentifier();
-});
