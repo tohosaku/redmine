9db996d252908ad41b70c3594fef9e35d1ef0e56
Takashi Kato <tohosaku@users.osdn.me>
2025-03-15 06:40:01 +0000

    Remove application-legacy.js, rails-ujs, jquery

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -6,8 +6,6 @@ import {metaContent,isVisible} from 'helper';
 // Turbo.session.drive = false;
 
 document.addEventListener("turbo:load", () => {
-  setupAjaxIndicator();
-  defaultFocus();
   // set default focus
   const element = document.querySelector('#content :focus')
   if ((element !== null) && (window.location.hash == '')) {
