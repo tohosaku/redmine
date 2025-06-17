f57b0e50d5497bfb360c549266e45b714e488570
Takashi Kato <tohosaku@users.osdn.me>
2025-06-17 04:54:54 +0000

    [WIP] Remove jQuery

--- app/javascript/controllers/queries/columns_controller.js
+++ app/javascript/controllers/queries/columns_controller.js
@@ -3,32 +3,10 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="queries--columns"
 export default class extends Controller {
   moveOptions(e) {
-    const from_id = e.params.from;
-    const to_id   = e.params.to;
-    $(this.element.form[from_id]).find('option:selected').detach().prop("selected", false).appendTo($(this.element.form[to_id]));
-  }
-
-  moveOptionTop(e) {
-    const theSel = this.element.form[e.params.id];
-    $(theSel).find('option:selected').detach().prependTo($(theSel));
-  }
-
-  moveOptionUp(e) {
-    const theSel = this.element.form[e.params.id];
-    $(theSel).find('option:selected').each(function(){
-      $(this).prev(':not(:selected)').detach().insertAfter($(this));
-    });
-  }
-
-  moveOptionDown(e) {
-    const theSel = this.element.form[e.params.id];
-    $($(theSel).find('option:selected').get().reverse()).each(function(){
-      $(this).next(':not(:selected)').detach().insertBefore($(this));
-    });
-  }
-
-  moveOptionBottom(e) {
-    const theSel = this.element.form[e.params.id];
-    $(theSel).find('option:selected').detach().appendTo($(theSel));
+    const from    = e.target.form.querySelector(`#${e.params.from}`);
+    const to      = e.target.form.querySelector(`#${e.params.to}`)
+    const selected = from.selectedOptions[0];
+    selected.selected = false;
+    to.append(selected);
   }
 }
