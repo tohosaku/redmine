a4145c9aa964b5e5e318db3f8b586b0831abfbe1
Takashi Kato <tohosaku@users.osdn.me>
2025-09-27 07:13:41 +0000

    test

---
+++ test/javascripts/controllers/inline_autocomplete_controller.test.js
@@ -0,0 +1,9 @@
+/* connect すると data-tribute="true" が付く
+
+app/javascript/controllers/inline_autocomplete_controller.js
+app/javascript/controllers/issue_relations/form_controller.js
+app/javascript/controllers/observe_autocomplete_controller.js
+app/javascript/controllers/timelog/bulk_edit_issue_controller.js
+app/javascript/controllers/timelog/form_issue_controller.js
+*/
+
